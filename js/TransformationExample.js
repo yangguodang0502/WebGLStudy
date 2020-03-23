7ac09c4e6e5314058fb3967286763f89ff8ae9fa
steve.yang <yangguodang@foxmail.com>
2020-03-23 21:04:01 +0800

    commit: modify index.html

--- js/TransformationExample.js
+++ js/TransformationExample.js
@@ -89,11 +89,11 @@ function setupShaders() {
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
     shaderProgram.uniformMVMatrix = gl.getUniformLocation(shaderProgram, "uMVMatrix")
-    shaderProgram.projectionMatrix = gl.getUniformLocation(shaderProgram, "uPMatrix")
+    shaderProgram.uniformProjMatrix = gl.getUniformLocation(shaderProgram, "uPMatrix")
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
-    modelViewMatrix = mat4.Create();
-    projectionMatrix = mat4.Create();
+    modelViewMatrix = mat4.create();
+    projectionMatrix = mat4.create();
     modelViewMatrixStack = [];
 }
 
@@ -103,104 +103,106 @@ function pushModelViewMatrix() {
 }
 
 function popModelViewMatrix() {
-    if (popModelViewMatrix.length == 0) {
+    if (modelViewMatrixStack.length == 0) {
         throw "Error, stack was empty";
     }
 
     modelViewMatrix = modelViewMatrixStack.pop();
 }
 
-function setupFloorBuffers() {
-    floorVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, floorVertexPositionBuffer);
-
-    var floorVertexPosition = [
-        // Plane in y=0
-         5.0,   0.0,  5.0,  //v0
-         5.0,   0.0, -5.0,  //v1
-        -5.0,   0.0, -5.0,  //v2
-        -5.0,   0.0,  5.0]; //v3
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(floorVertexPosition), gl.STATIC_DRAW);
-    floorVertexPositionBuffer.itemSize = 3;
-    floorVertexPositionBuffer.numberOfItems = 4;
-
-    floorVertexIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, floorVertexIndexBuffer);
-
-    var floorVertexIndices = [0, 1, 2, 3]; 
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(floorVertexIndices), gl.STATIC_DRAW);
-    floorVertexIndexBuffer.itemSize = 1;
-    floorVertexIndexBuffer.numberOfItems = 4;
+function setupFloorBuffers() {   
+  floorVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, floorVertexPositionBuffer);
+  
+  var floorVertexPosition = [
+      // Plane in y=0
+       5.0,   0.0,  5.0,  //v0
+       5.0,   0.0, -5.0,  //v1
+      -5.0,   0.0, -5.0,  //v2
+      -5.0,   0.0,  5.0]; //v3
+  
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(floorVertexPosition),
+                gl.STATIC_DRAW);
+  
+  floorVertexPositionBuffer.itemSize = 3;
+  floorVertexPositionBuffer.numberOfItems = 4;
+  
+  floorVertexIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, floorVertexIndexBuffer);
+  var floorVertexIndices = [0, 1, 2, 3];  
+            
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(floorVertexIndices), 
+                gl.STATIC_DRAW);
+  floorVertexIndexBuffer.itemSize = 1;
+  floorVertexIndexBuffer.numberOfItems = 4;
 }
 
 function setupCubeBuffers() {
-    cubeVertexPositionBUffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBUffer);
+  cubeVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+  
+  var cubeVertexPosition = [
+       // Front face
+       1.0,  1.0,  1.0, //v0
+      -1.0,  1.0,  1.0, //v1
+      -1.0, -1.0,  1.0, //v2
+       1.0, -1.0,  1.0, //v3
 
-    var cubeVertexPosition = [
-        // Front face
-        1.0,  1.0,  1.0, //v0
-       -1.0,  1.0,  1.0, //v1
-       -1.0, -1.0,  1.0, //v2
-        1.0, -1.0,  1.0, //v3
- 
-        // Back face
-        1.0,  1.0, -1.0, //v4
-       -1.0,  1.0, -1.0, //v5
-       -1.0, -1.0, -1.0, //v6
-        1.0, -1.0, -1.0, //v7
-        
-        // Left face
-       -1.0,  1.0,  1.0, //v8
-       -1.0,  1.0, -1.0, //v9
-       -1.0, -1.0, -1.0, //v10
-       -1.0, -1.0,  1.0, //v11
-        
-        // Right face
-        1.0,  1.0,  1.0, //12
-        1.0, -1.0,  1.0, //13
-        1.0, -1.0, -1.0, //14
-        1.0,  1.0, -1.0, //15
-        
-         // Top face
-         1.0,  1.0,  1.0, //v16
-         1.0,  1.0, -1.0, //v17
-        -1.0,  1.0, -1.0, //v18
-        -1.0,  1.0,  1.0, //v19
-        
-         // Bottom face
-         1.0, -1.0,  1.0, //v20
-         1.0, -1.0, -1.0, //v21
-        -1.0, -1.0, -1.0, //v22
-        -1.0, -1.0,  1.0, //v23
-   ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertexPosition), gl.STATIC_DRAW);
-    cubeVertexPositionBUffer.itemSize = 3;
-    cubeVertexPositionBUffer.numberOfItems = 24;
-
-    cubeVertexIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-
-    var cubeVertexIndices = [
-        0, 1, 2,      0, 2, 3,    // Front face
-        4, 6, 5,      4, 7, 6,    // Back face
-        8, 9, 10,     8, 10, 11,  // Left face
-        12, 13, 14,   12, 14, 15, // Right face
-        16, 17, 18,   16, 18, 19, // Top face
-        20, 22, 21,   20, 23, 22  // Bottom face
-    ];
-
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
-    floorVertexIndexBuffer.itemSize = 1;
-    floorVertexIndexBuffer.numberOfItems = 36;
+       // Back face
+       1.0,  1.0, -1.0, //v4
+      -1.0,  1.0, -1.0, //v5
+      -1.0, -1.0, -1.0, //v6
+       1.0, -1.0, -1.0, //v7
+       
+       // Left face
+      -1.0,  1.0,  1.0, //v8
+      -1.0,  1.0, -1.0, //v9
+      -1.0, -1.0, -1.0, //v10
+      -1.0, -1.0,  1.0, //v11
+       
+       // Right face
+       1.0,  1.0,  1.0, //12
+       1.0, -1.0,  1.0, //13
+       1.0, -1.0, -1.0, //14
+       1.0,  1.0, -1.0, //15
+       
+        // Top face
+        1.0,  1.0,  1.0, //v16
+        1.0,  1.0, -1.0, //v17
+       -1.0,  1.0, -1.0, //v18
+       -1.0,  1.0,  1.0, //v19
+       
+        // Bottom face
+        1.0, -1.0,  1.0, //v20
+        1.0, -1.0, -1.0, //v21
+       -1.0, -1.0, -1.0, //v22
+       -1.0, -1.0,  1.0, //v23
+  ];
+  
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertexPosition),
+                gl.STATIC_DRAW);
+  cubeVertexPositionBuffer.itemSize = 3;
+  cubeVertexPositionBuffer.numberOfItems = 24; 
+  
+  cubeVertexIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+  var cubeVertexIndices = [
+            0, 1, 2,      0, 2, 3,    // Front face
+            4, 6, 5,      4, 7, 6,    // Back face
+            8, 9, 10,     8, 10, 11,  // Left face
+            12, 13, 14,   12, 14, 15, // Right face
+            16, 17, 18,   16, 18, 19, // Top face
+            20, 22, 21,   20, 23, 22  // Bottom face
+        ];
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), 
+                gl.STATIC_DRAW);
+  cubeVertexIndexBuffer.itemSize = 1;
+  cubeVertexIndexBuffer.numberOfItems = 36;
 }
 
 function setupBuffers() {
-    setupFloorBuffers();
-    setupCubeBuffers();
+  setupFloorBuffers();
+  setupCubeBuffers();
 }
 
 function uploadModelViewMatrixToShader() {
@@ -211,13 +213,72 @@ function uploadProjectionMatrixToShader() {
     gl.uniformMatrix4fv(shaderProgram.uniformProjMatrix, false, projectionMatrix);
 }
 
+function drawFloor(r,g,b,a) {
+    gl.disableVertexAttribArray(shaderProgram.vertexColorAttribute);
+    gl.vertexAttrib4f(shaderProgram.vertexColorAttribute, r, g, b, a);
+    gl.bindBuffer(gl.ARRAY_BUFFER, floorVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, floorVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, floorVertexIndexBuffer);
+    gl.drawElements(gl.TRIANGLE_FAN, floorVertexIndexBuffer.numberOfItems, gl.UNSIGNED_SHORT, 0);
+}
+
+function drawCube(r,g,b,a) {
+    gl.disableVertexAttribArray(shaderProgram.vertexColorAttribute);
+    gl.vertexAttrib4f(shaderProgram.vertexColorAttribute, r, g, b, a);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numberOfItems, gl.UNSIGNED_SHORT, 0);
+}
+
+function drawTable() {
+    pushModelViewMatrix();
+    mat4.translate(modelViewMatrix, [0.0, 1.0, 0.0], modelViewMatrix);
+    mat4.scale(modelViewMatrix, [2.0, 0.1, 2.0], modelViewMatrix);
+    uploadModelViewMatrixToShader();
+
+    drawCube(0.72, 0.53, 0.04, 1.0);
+    popModelViewMatrix();
+
+    for (let i = -1; i <= 1; i += 2) {
+        for (let j = -1; j <= 1; j += 2) {
+            pushModelViewMatrix();
+            mat4.translate(modelViewMatrix, [i*1.9, -0.1, j*1.9], modelViewMatrix);
+            mat4.scale(modelViewMatrix, [0.1, 1.0, 0.1], modelViewMatrix);
+            uploadModelViewMatrixToShader();
+            drawCube(0.72, 0.53, 0.04, 1.0);
+            popModelViewMatrix();
+        }        
+    }
+}
+
 function draw() {
     gl.viewport(0,0, gl.viewportWidth, gl.viewportHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexBuffer.positionSize, gl.FLOAT, false, 16, 0);
-    gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, vertexBuffer.colorSize, gl.UNSIGNED_BYTE, true, 16, 12);
-    gl.drawArrays(gl.TRIANGLES, 0, vertexBuffer.numberOfItems);    
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    mat4.perspective(60, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, projectionMatrix);
+    mat4.identity(modelViewMatrix);
+    mat4.lookAt([8, 5, -10],[0, 0, 0], [0, 1, 0], modelViewMatrix);
+    
+    uploadModelViewMatrixToShader();
+    uploadProjectionMatrixToShader();
+    
+    // Draw floor in red color
+    drawFloor(1.0, 0.0, 0.0, 1.0);
+    
+    // Draw table
+    pushModelViewMatrix();
+    mat4.translate(modelViewMatrix, [0.0, 1.1, 0.0], modelViewMatrix);
+    uploadModelViewMatrixToShader();
+    drawTable();
+    popModelViewMatrix();
+
+    // Draw box on top of the table
+    pushModelViewMatrix();
+    mat4.translate(modelViewMatrix, [0.0, 2.7, 0.0], modelViewMatrix);
+    mat4.scale(modelViewMatrix, [0.5, 0.5, 0.5], modelViewMatrix);
+    uploadModelViewMatrixToShader();
+    drawCube(0.0, 0.0, 1.0, 1.0);
+    popModelViewMatrix();
 }
 
 function startup() {
@@ -226,5 +287,7 @@ function startup() {
     setupShaders();
     setupBuffers();
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
     draw();
 }
